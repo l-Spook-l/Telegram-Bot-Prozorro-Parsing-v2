80ef917c086c212817d12f96e991fe8e453453d1
l-Spook-l <uaspookua@gmail.com>
2024-08-06 18:30:53 +0300

    Added custom menu

---
+++ common/bot_cmds_list.py
@@ -0,0 +1,6 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command="help", description="Інструкція використання бота"),
+    BotCommand(command="general_menu", description="Показати загальні кнопки для користування бота"),
+]
